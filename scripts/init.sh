2e0f15f3aaca9e8736f0ceaec2f37904881f1fe6
outofsleep <96475405+outofsleep@users.noreply.github.com>
2022-04-05 19:34:32 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -15,14 +15,9 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install cmake pkg-config openssl git llvm
 fi
 
-if [[ $(cargo --version) ]]; then
-    echo "Found cargo"
-else
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
-    source $HOME/.cargo/env
-    export PATH=$HOME/.cargo/bin:$PATH
-fi
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
+export PATH=$HOME/.cargo/bin:$PATH
 rustup toolchain add $WASM_BUILD_TOOLCHAIN
 rustup default $WASM_BUILD_TOOLCHAIN
 rustup target add wasm32-unknown-unknown --toolchain $WASM_BUILD_TOOLCHAIN
